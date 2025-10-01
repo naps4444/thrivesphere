207eb7448dffe58fb52de69e02dc8b3cec7a782f
naps4444 <uchemadunmerengwa@gmail.com>
2025-10-01 10:31:22 +0100

    added google analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,12 +6,34 @@ export default function Document() {
       <Head>
         {/* Preconnect to Google Fonts for better performance */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+
         {/* Google Fonts */}
         <link
           href="https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&family=Cinzel:wght@400;700&family=Lusitana:wght@400;700&family=Rubik+Maps&family=Volkhov:wght@400;700&family=Rakkas&family=Rage+Italic&display=swap"
           rel="stylesheet"
+        />
+
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-T9YVRJHVBG"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-T9YVRJHVBG', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
         />
       </Head>
       <body className="antialiased">
